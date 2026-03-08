95c088887d655d6f03c80fbd651c38e400b856a8
Sahil Solanki <sahilsolanki116@gmail.com>
2026-03-08 09:43:16 +0530

    Updated files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("This is a demo");
+import "dotenv/config";
+import mongoose from "mongoose";
+import { DB_NAME } from "./contants.js";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("MongoDB Atlas Successfully Connnected");
+  } catch (error) {
+    console.error("MONGODB Connection Failed: ", error);
+    process.exit(1);
+  }
+};
+connectDB();
